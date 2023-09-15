d853cb7ec6ea6a085d3fe43f88c4dac4531cd36d
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-09-15 23:59:14 +0300

    add owner create form for first time

--- src/views/create.tsx
+++ src/views/create.tsx
@@ -1,5 +1,14 @@
+import ConfigurationLayout from '@/layouts/ConfigurationLayout'
+import DefaultLayout from '@/layouts/DefaultLayout'
+import { useTranslation } from 'react-i18next'
+
 const OwnerCreateView = () => {
-  return <></>
+  const { t } = useTranslation('select')
+  return (
+    <ConfigurationLayout title={t('meta.title')} description={t('meta.description')} keywords={t('meta.keywords')}>
+      <DefaultLayout></DefaultLayout>
+    </ConfigurationLayout>
+  )
 }
 
 export default OwnerCreateView
