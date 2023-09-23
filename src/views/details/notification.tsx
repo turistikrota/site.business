93deeec907d2ae4ac879a971c6ecbd2efae75569
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-09-23 22:53:32 +0300

    add notification view

---
+++ src/views/details/notification.tsx
@@ -0,0 +1,17 @@
+import MetaWrapper from '@/components/MetaWrapper'
+import { useTranslation } from 'react-i18next'
+
+function OwnershipNotificationView() {
+  const { t } = useTranslation('notification')
+  return (
+    <MetaWrapper title={t('meta.title')} description={t('meta.description')} keywords={t('meta.keywords')}>
+      <section className='sm:max-w-md mx-auto min-h-screen h-full flex items-start justify-center'>
+        notification vieww
+      </section>
+    </MetaWrapper>
+  )
+}
+
+OwnershipNotificationView.displayName = 'OwnershipNotificationView'
+
+export { OwnershipNotificationView as Component }
