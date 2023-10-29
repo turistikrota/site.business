1976eb1ed4d5591905b5a8bdbc27ceccf75d8367
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-10-29 18:14:05 +0300

    feature/design: fix layout

--- src/layouts/ConfigurationLayout.tsx
+++ src/layouts/ConfigurationLayout.tsx
@@ -1,4 +1,5 @@
 import MetaWrapper from '@/components/MetaWrapper'
+import GlassEffect from '@turistikrota/ui/design/glass'
 import Logo from '@turistikrota/ui/logo'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
@@ -23,10 +24,12 @@ export default function ConfigurationLayout({
       <section className='h-full'>
         <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto h-full lg:py-0'>
           <Link to={`https://turistikrota.com/${i18n.language}`} target='_blank' className='flex items-center mb-6'>
-            <Logo>
-              <Logo.Badge>beta</Logo.Badge>
-            </Logo>
+            <Logo />
           </Link>
+          <GlassEffect justify='center' align='center'>
+            <GlassEffect.Item color='bg-primary' size='lg' position='-ml-20 mt-60' />
+            <GlassEffect.Item color='bg-secondary' size='xl' position='ml-10 mt-20' />
+          </GlassEffect>
           <div className='w-full bg-second shadow-lg rounded-lg md:mt-0 sm:max-w-md xl:p-0'>{children}</div>
         </div>
       </section>
